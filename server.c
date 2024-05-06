2f5bc4ad877bda5184015e789db8f0903ff2c9b0
Eduardo Hernandez <112764648+eduzeu@users.noreply.github.com>
2024-05-06 14:02:23 -0400

    Update server.c

--- server.c
+++ server.c
@@ -1,9 +1,3 @@
-/*******************************************************************************
- * Name        : server.c
- * Author      : Eduardo Hernandez
- * Pledge      : I pledge my honor that I have abided by the Stevens Honor System. 
- ******************************************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
